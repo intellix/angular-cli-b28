88f79c2000d79c6d070100709533a81804b80be3
Dominic Watson <domwatson@live.co.uk>
2017-02-01 23:13:33 +0100

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/lazy/lazy-routing.module.ts
@@ -0,0 +1,17 @@
+import { LazyComponent } from './lazy.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LazyComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class LazyRoutingModule { }
